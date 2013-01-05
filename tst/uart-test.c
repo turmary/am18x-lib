06f7ce0d7b6f58a3692c34b54c59112d1c60bcec
yanglsh <yanglsh@9fcba094-cb13-cc49-8166-e0e6f87de84e>
2013-01-05 15:59:41 +0000

    提交UART寄存器写测试代码
    git-svn-id: http://tary-think:8080/svn/am1808/am18x-lib@34 9fcba094-cb13-cc49-8166-e0e6f87de84e

---
+++ tst/uart-test.c
@@ -0,0 +1,38 @@
+// tary, 23:32 2013/1/5
+#include <stdio.h>
+#include "am18x_lib.h"
+
+UART_con_t uart_reg[1] = {
+{
+	0,		// RBR
+	0,		// IER
+	0x0001,		// IIR
+	0,		// LCR
+
+	0,		// MCR
+	0x60,		// LSR
+	0,		// MSR
+	0,		// SCR
+
+	0,{0,0,0},	// DLL
+	0,{0,0,0},	// DLH
+	{0x11020002,	// REVID1
+	0},		// REVID2
+
+	0x0002,		// PWREMU_MGMT
+	0,		// MDR
+},
+};
+
+int main(int argc, char* argv[]) {
+	uart_conf_t conf;
+	int i;
+
+	uart_init_conf(&conf);
+	uart_set_conf(uart_reg, &conf);
+
+	for (i = 0; i < sizeof(uart_reg) / sizeof (unsigned); i++) {
+		printf("reg[%2d] = 0x%.8X\n", i, ((unsigned*)uart_reg)[i]);
+	}
+	return 0;
+}
