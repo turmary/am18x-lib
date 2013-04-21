f86033c44b86d376d3bee99f35941da56e5455bb
yanglsh <yanglsh@9fcba094-cb13-cc49-8166-e0e6f87de84e>
2013-04-21 16:54:40 +0000

    更新PLL演示代码
    git-svn-id: http://tary-think:8080/svn/am1808/am18x-lib@96 9fcba094-cb13-cc49-8166-e0e6f87de84e

--- pll/main.c
+++ pll/main.c
@@ -26,11 +26,26 @@ kv_t reset_sources[] = {
 	KV(PLL_RESET_POWER_ON),
 };
 
+int check_delay(void) {
+	uint32_t start_minutes, stop_minutes;
+
+	start_minutes = systick_elapsed();
+	delay(delay_count);
+	stop_minutes = systick_elapsed();
+	printk("delay start: %d ms\n", start_minutes);
+	printk("delay stop : %d ms\n", stop_minutes);
+	printk("delay used : %d ms\n", stop_minutes - start_minutes);
+	printk("\n\n");
+	return 0;
+}
+
 int main(int argc, char* argv[]) {
 	const char* title = "\nam18x library for pll!\n";
-	uint32_t start_minutes, stop_minutes;
 	pll_reset_t reset;
 	int i;
+
+	arm_intr_enable();
+	systick_start();
 
 	printk(title);
 	printk("tary, compiled date : %s %s\n", __DATE__, __TIME__);
@@ -42,18 +57,14 @@ int main(int argc, char* argv[]) {
 	printk("RSTYPE = 0x%.8X\n", PLL0->RSTYPE);
 	printk("Reset by %s\n", reset_sources[reset].val);
 
+	check_delay();
+
 	pll_conf(PLL0, pllconf);
 	clk_node_recalc_freq();
 	uart_init();
 
-	arm_intr_enable();
-	systick_start();
-
-	start_minutes = systick_elapsed();
-	delay(delay_count);
-	stop_minutes = systick_elapsed();
-	printk("delay start: %d ms\n", start_minutes);
-	printk("delay stop : %d ms\n", stop_minutes);
+	printk("After dll opened\n");
+	check_delay();
 
 	pll_cmd(PLL0, PLL_CMD_SOFT_RESET, 0);
 
