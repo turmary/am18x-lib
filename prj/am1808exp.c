391ee651ea53e0bbc96b9e5bbbc7d718cabf20a0
yanglsh <yanglsh@9fcba094-cb13-cc49-8166-e0e6f87de84e>
2013-04-29 09:58:00 +0000

    BUFF_OEn控制失败
    git-svn-id: http://tary-think:8080/svn/am1808/am18x-lib@115 9fcba094-cb13-cc49-8166-e0e6f87de84e

--- prj/am1808exp.c
+++ prj/am1808exp.c
@@ -6,6 +6,22 @@
 
 #define SYSTICK_PERIOD			10/* milli seconds */
 
+// 1015640A_AM1808_SOM-M1_Schematic.pdf
+#define BUFF_OEn			GPIO_BANK2,GPIO_PIN_6
+
+static int buff_en_enable(am18x_bool on_noff) {
+	uint32_t v = on_noff? GPIO_LOW: GPIO_HIGH;
+
+	psc_state_transition(PSC_GPIO, PSC_STATE_ENABLE);
+
+	gpio_set_mux(BUFF_OEn, GPIO_DIR_OUTPUT);
+	gpio_set_output1(BUFF_OEn, v);
+
+	// gpio_set_mux(BUFF_OEn, GPIO_DIR_INPUT);
+	// printk("BUFF_OEn = %d\n", gpio_get_output1(BUFF_OEn));
+	return 0;
+}
+
 int low_level_init(void) {
 	extern int isr_init(void);
 	extern int output_a_char(int);
@@ -30,6 +46,8 @@ int low_level_init(void) {
 		return r;
 	}
 */
+	// invalid operation
+	buff_en_enable(AM18X_FALSE);
 
 #ifdef _M_BOOT_DELAY
 	delay(284091 * 3);
