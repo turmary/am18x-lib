d3518d87f9262c4edc0e6ef8b8b74fd5d39868a2
yanglsh <yanglsh@9fcba094-cb13-cc49-8166-e0e6f87de84e>
2016-06-04 17:25:16 +0000

    修正strcmp返回值的Bug Again
    git-svn-id: http://tary-think:8080/svn/am1808/am18x-lib@275 9fcba094-cb13-cc49-8166-e0e6f87de84e

--- lib/auxlib.c
+++ lib/auxlib.c
@@ -72,8 +72,8 @@ size_t strnlen(const char* s, size_t size) {
 }
 
 int strcmp(const char* ss, const char* sd) {
-	while (*ss && *sd && *ss++ == *sd++);
-	return *--ss - *--sd;
+	for (;*ss && *sd && *ss == *sd; ss++, sd++);
+	return *ss - *sd;
 }
 
 const char* strchr(const char* s, char c) {
